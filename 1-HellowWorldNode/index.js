56e1e060c4ab2209063f1876af25a1c75d2cee7a
Özlem ŞEN <ozlem.sen@gmail.com>
2026-04-06 11:52:41 +0300

    Add console logs and setTimeout example

--- 1-HellowWorldNode/index.js
+++ 1-HellowWorldNode/index.js
@@ -1,1 +1,10 @@
 
+console.log("Hello node js.");
+const array = [1,2,3,4];
+console.log(array, 'array');
+
+setTimeout(()=> {
+    console.log('this message is delayed for two hours.');
+} , 2000);
+
+console.log("this is the last line of sync code");
